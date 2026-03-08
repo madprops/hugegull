2597449c305479ebab5dae4609b511ea7bb3fb24
Auric Vente <mprops@protonmail.com>
2026-03-07 22:23:13 -0600

    Mods

--- src/engine.py
+++ src/engine.py
@@ -293,7 +293,6 @@ class Engine:
             utils.error(result.stderr)
         else:
             shutil.rmtree(config.project_dir, ignore_errors=True)
-            os.rmdir(config.project_dir)
             utils.info(f"Saved: {self.file}")
 
     def get_stream_duration(self) -> None:
